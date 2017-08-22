f7f717034e0f906091d411c0c115794612152bb0
wwgoodstudy <wwwangwei623@126.com>
2017-08-22 17:35:42 +0800

    test

--- WpfTest/MainWindow.xaml.cs
+++ WpfTest/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfTest
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
     }
 }
